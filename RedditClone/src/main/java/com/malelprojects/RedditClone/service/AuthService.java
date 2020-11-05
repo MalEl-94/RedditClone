2f7cd869aa7e4bf22fc92626ea074e2fd37f2689
ahadM <ahadm94@hotmail.co.uk>
2020-11-05 21:53:37 +0000

    Access token method added in AuthService. MailContentBuilder class added. mailTemplate added. Thymeleaf dependency added.

--- RedditClone/src/main/java/com/malelprojects/RedditClone/service/AuthService.java
+++ RedditClone/src/main/java/com/malelprojects/RedditClone/service/AuthService.java
@@ -2,7 +2,9 @@ package com.malelprojects.RedditClone.service;
 
 import com.malelprojects.RedditClone.dto.RegisterRequest;
 import com.malelprojects.RedditClone.model.User;
+import com.malelprojects.RedditClone.model.VerificationToken;
 import com.malelprojects.RedditClone.repository.UserRepository;
+import com.malelprojects.RedditClone.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -18,7 +21,7 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-
+    private final VerificationTokenRepository verificationTokenRepository;
     @Transactional
     public void signup(RegisterRequest registerRequest){
         User user = new User();
@@ -29,6 +32,17 @@ public class AuthService {
         user.setEnabled(false);
         userRepository.save(user);
 
+        String token = generateVerificationToken(user);
+    }
 
+
+    private String generateVerificationToken(User user){
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
