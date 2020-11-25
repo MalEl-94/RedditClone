a2751706f2d2ce5f070bd90374c91d6aeb4570c4
ahadM <ahadm94@hotmail.co.uk>
2020-11-25 16:34:49 +0000

    UserDetailsServiceImp class added.

--- RedditClone/src/main/java/com/malelprojects/RedditClone/congif/SecurityConfig.java
+++ RedditClone/src/main/java/com/malelprojects/RedditClone/congif/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.malelprojects.RedditClone.congif;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    //provides user data
+    private final UserDetailsService userDetailsService;
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception{
@@ -19,6 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
 
---
+++ RedditClone/src/main/java/com/malelprojects/RedditClone/service/UserDetailsServiceImp.java
@@ -0,0 +1,44 @@
+package com.malelprojects.RedditClone.service;
+
+import com.malelprojects.RedditClone.model.User;
+import com.malelprojects.RedditClone.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.graalvm.compiler.lir.LIRInstruction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.net.UnknownServiceException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImp implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    //overrides springs user details method
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Retrieve user based on username
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        //If not found throw exception
+        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("No user"+
+                        "found with username : " + username));
+         return new org.springframework.security.
+                 core.userdetails.User(user.getUsername(),user.getPassword(),
+                 user.isEnabled(),true,true,
+                 true, getAuthorities("USER"));
+    }
+
+    //Authority for user
+    private Collection<? extends GrantedAuthority> getAuthorities(String role){
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+}
